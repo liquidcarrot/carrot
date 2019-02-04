a87a2469a2e3d39e5a213688ac9ab1b81614247d
luiscarbonell <luiscarbonell5683@gmail.com>
2019-02-04 18:45:31 +0000

    Exports Building Blocks

---
+++ src/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let Neuron = require('./neuron')
+let Connection = require('./connection')
+let Layer = require('./layer')
+let Network = require('./network')
+
+module.exports = {
+  Neuron,
+  Connection,
+  Layer,
+  Network
+}
