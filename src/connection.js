0b511ec3aeaaa0ccaca4a6d01c579c4b9c6bea9b
luiscarbonell <luiscarbonell5683@gmail.com>
2019-02-25 06:37:04 +0000

    Working Neurons and Connection (Synchronous)

--- src/connection.js
+++ src/connection.js
@@ -1,35 +1,13 @@
 'use strict'
 
+let _ = require('lodash')
 let faker = require('faker')
-let math = require('mathjs')
+let chalk = require('chalk')
+let async = require('neo-async')
+let Promise = require('bluebird')
 
-let random = {
-  sign: () => Math.random() < 0.5 ? -1 : 1,
-  number: () => math.bignumber(random.sign() * faker.random.number())
-}
-
-let Connection = function(props, options) {
-  let self = this
-  
-  self.from = props ? (props.from || undefined) : undefined
-  self.to = props ? (props.to || undefined) : undefined
-  self.weight = math.bignumber(props ? (props.weight || random.number()) : random.number())
-  
-  // For aynchronous/non-blocking networks
-  /*
-  self.forward = {
-    queue: [],
-    states: []
-  }
-  self.backward = {
-    queue: [],
-    states: []
-  }
-  
-  if(props) {
-    self.forward = props.forward || self.forward
-    self.backward = props.backward || self.backward
-  }*/
+let Connection = function({ from, to, weight = Math.random() * 2 - 1 } = {}) {
+  _.assignIn(this, { from, to, weight })
 }
 
 module.exports = Connection
