d0cf72ed02d04df07b393187abc2b10d645e3be8
luiscarbonell <luiscarbonell5683@gmail.com>
2019-03-01 06:41:01 +0000

    Basic Architectures - Placeholders

---
+++ src/architecture.js
@@ -0,0 +1,27 @@
+let _ = require('lodash')
+
+let Network = require('./network');
+let Layer = require('./layer');
+
+let Perceptron = function() {
+  
+}
+
+let Liquid = function(inputs, hidden, outputs, connections, gates) {
+  
+}
+
+let LSTM = function() {
+  
+}
+
+let Hopfield = function(size) {
+  
+}
+
+module.exports = {
+  Perceptron,
+  LSTM,
+  Liquid,
+  Hopfield
+}
