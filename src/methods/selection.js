cadd37790bd04d9bc3476ddfa01eeee65cbe7666
Christian George Echevarria-Reina <christian.george.echevarria@gmail.com>
2019-03-29 18:56:00 +0000

    Add JSDoc Description to Selection.js

--- src/methods/selection.js
+++ src/methods/selection.js
@@ -1,20 +1,21 @@
 /**
  *
+ * Genetic Algorithm Selection Methods (Genetic Operator)
+ *
  * @module
  *
  * @namespace
  *
- * @todo Create a namespace description
  * @todo Add `@prop` tag descriptions
  *
  * @see {@link https://en.wikipedia.org/wiki/Selection_(genetic_algorithm)|Selection (genetic algorithm)}
  *
- * @prop {object} FITNESS_PROPORTIONATE @see {@link https://en.wikipedia.org/wiki/Fitness_proportionate_selection|Fitness Proportionate Selection / Roulette Wheel Selection}
+ * @prop {object} FITNESS_PROPORTIONATE {@link https://en.wikipedia.org/wiki/Fitness_proportionate_selection|Fitness Proportionate / Roulette Wheel Selection on Wikipedia}
  * @prop {string} FITNESS_PROPORTIONATE.name="FITNESS_PROPORTIONATE"
  * @prop {object} POWER
  * @prop {string} POWER.name="POWER"
  * @prop {number} POWER.power=4
- * @prop {object} TOURNAMENT @see {@link https://en.wikipedia.org/wiki/Tournament_selection|Tournament Selection}
+ * @prop {object} TOURNAMENT {@link https://en.wikipedia.org/wiki/Tournament_selection|Tournament Selection on Wikipedia}
  * @prop {string} TOURNAMENT.name="TOURNAMENT"
  * @prop {number} TOURNAMENT.size=5
  * @prop {number} TOURNAMENT.probability=0.5
