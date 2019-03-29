a9f8f412d40a16489244f4c332a6b3f75253d178
Christian George Echevarria-Reina <christian.george.echevarria@gmail.com>
2019-03-29 18:39:47 +0000

    Selection.js Documentation 2nd Pass

--- src/methods/selection.js
+++ src/methods/selection.js
@@ -1,24 +1,27 @@
 /**
- * 
+ *
  * @module
- * 
+ *
  * @todo Create a namespace description
- * @todo Add `@prop` tag types
  * @todo Add `@prop` tag descriptions
- * @todo Add `@prop` tag defaults
- * 
- * @see {@link // https://en.wikipedia.org/wiki/Selection_(genetic_algorithm)|Selection (genetic algorithm)}
- * 
+ *
+ * @see {@link https://en.wikipedia.org/wiki/Selection_(genetic_algorithm)|Selection (genetic algorithm)}
+ *
+ * @typedef selection_method
+ * @type {object}
+ *
  * @prop {object} FITNESS_PROPORTIONATE
  * @prop {string} FITNESS_PROPORTIONATE.name="FITNESS_PROPORTIONATE"
  * @prop {object} POWER
  * @prop {string} POWER.name="POWER"
+ * @prop {number} POWER.power=4
  * @prop {object} TOURNAMENT
  * @prop {string} TOURNAMENT.name="TOURNAMENT"
- * @prop {object} TOURNAMENT.size=5
- * @prop {object} TOURNAMENT.probability=0.5
+ * @prop {number} TOURNAMENT.size=5
+ * @prop {number} TOURNAMENT.probability=0.5
 */
 var selection = {
+  // @see {@link https://en.wikipedia.org/wiki/Fitness_proportionate_selection|Fitness Proportionate Selection / Roulette Wheel Selection}
   FITNESS_PROPORTIONATE: {
     name: 'FITNESS_PROPORTIONATE'
   },
@@ -26,6 +29,7 @@ var selection = {
     name: 'POWER',
     power: 4
   },
+  // @see {@link https://en.wikipedia.org/wiki/Tournament_selection|Tournament Selection}
   TOURNAMENT: {
     name: 'TOURNAMENT',
     size: 5,
