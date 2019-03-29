e03cd412b33a803b47031904be54ab6b24d9bf81
Christian George Echevarria-Reina <christian.george.echevarria@gmail.com>
2019-03-29 18:50:01 +0000

    Add @see JSDoc Tag to Prop Description Area

--- src/methods/selection.js
+++ src/methods/selection.js
@@ -9,20 +9,17 @@
  *
  * @see {@link https://en.wikipedia.org/wiki/Selection_(genetic_algorithm)|Selection (genetic algorithm)}
  *
- * @prop {object} FITNESS_PROPORTIONATE
+ * @prop {object} FITNESS_PROPORTIONATE @see {@link https://en.wikipedia.org/wiki/Fitness_proportionate_selection|Fitness Proportionate Selection / Roulette Wheel Selection}
  * @prop {string} FITNESS_PROPORTIONATE.name="FITNESS_PROPORTIONATE"
  * @prop {object} POWER
  * @prop {string} POWER.name="POWER"
  * @prop {number} POWER.power=4
- * @prop {object} TOURNAMENT
+ * @prop {object} TOURNAMENT @see {@link https://en.wikipedia.org/wiki/Tournament_selection|Tournament Selection}
  * @prop {string} TOURNAMENT.name="TOURNAMENT"
  * @prop {number} TOURNAMENT.size=5
  * @prop {number} TOURNAMENT.probability=0.5
 */
 var selection = {
-  /**
-   * @see {@link https://en.wikipedia.org/wiki/Fitness_proportionate_selection|Fitness Proportionate Selection / Roulette Wheel Selection}
-   */
   FITNESS_PROPORTIONATE: {
     name: 'FITNESS_PROPORTIONATE'
   },
@@ -30,9 +27,6 @@ var selection = {
     name: 'POWER',
     power: 4
   },
-  /**
-   * @see {@link https://en.wikipedia.org/wiki/Tournament_selection|Tournament Selection}
-   */
   TOURNAMENT: {
     name: 'TOURNAMENT',
     size: 5,
