cf92495f5d61cb565eee4e08db396b97a90b84d3
Christian George Echevarria-Reina <christian.george.echevarria@gmail.com>
2019-04-06 22:09:34 +0000

    Add Descriptions & Examples to Gating.js

--- src/methods/gating.js
+++ src/methods/gating.js
@@ -1,26 +1,47 @@
 /**
-* Specifies how to gate a connection between two groups of multiple neurons
+* Specifies how to gate a connection between two groups of multiple neurons.
+* Gating makes the weights in networks more dynamic by adapting them to their gating node. Read more about it [here](https://en.wikipedia.org/wiki/Synaptic_gating). For specific implementations of gating, see [Node](Node), [Group](Group) and [Network](Network)
 *
 * @namespace
 *
-* @prop {object} OUTPUT
-* @prop {string} OUTPUT.name="OUTPUT"
-* @prop {object} INPUT
-* @prop {string} INPUT.name="INPUT"
-* @prop {object} SELF
-* @prop {string} SELF.name="SELF"
 */
 var gating = {
+  /**
+   * @constant
+   * @type {object}
+   * @description Every node in the gating group will gate (at least) 1 node in the emitting group and all its connections to the other, receiving group
+   * @default
+   *
+   * @example
+   * myNetwork.mutate(methods.mutation.ADD_NODE);
+   */
   OUTPUT: {
     name: 'OUTPUT'
   },
+  /**
+   * @constant
+   * @type {object}
+   * @description Every node in the gating group will gate (at least) 1 node in the receiving group and all its connections from the other, emitting group
+   * @default
+   *
+   * @example
+   * myNetwork.mutate(methods.mutation.ADD_NODE);
+   */
   INPUT: {
     name: 'INPUT'
   },
+  /**
+   * @constant
+   * @type {object}
+   * @description Every node in the gating group will gate (at least) 1 self connection in the emitting/receiving group
+   * @default
+   *
+   * @example
+   * myNetwork.mutate(methods.mutation.ADD_NODE);
+   */
   SELF: {
     name: 'SELF'
   }
 };
 
-/* Export */
 module.exports = gating;
