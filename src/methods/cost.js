d8258d5bbdbc88a85db9c76e09f7f38afc31ca48
Christian George Echevarria-Reina <christian.george.echevarria@gmail.com>
2019-04-06 19:12:01 +0000

    Add Cost.js Examples & Descriptions

--- src/methods/cost.js
+++ src/methods/cost.js
@@ -1,25 +1,26 @@
-/*******************************************************************************
-                                    COST FUNCTIONS
-*******************************************************************************/
-
 /**
+ * @namespace cost
  *
  * @see {@link https://en.wikipedia.org/wiki/Loss_function|Loss Function on Wikipedia}
- * @namespace cost
  */
 var cost = {
   /**
   * Cross entropy error
   *
-  * @todo Add `@param` tag description
-  * @todo Add `@returns` tag type
-  * @todo Add `@returns` tag description
-  *
   * @see {@link http://bit.ly/2p5W29A|Cross-entropy Error Function}
-  * @param {number} target
-  * @param {number[]} output
+  * @param {number} target Ideal value
+  * @param {number[]} output Actual values
   *
-  * @returns {number} - Mean squared error
+  * @returns {number} [Cross entropy error](https://ml-cheatsheet.readthedocs.io/en/latest/loss_functions.html)
+  *
+  * @example
+  * myNetwork.train(trainingData, {
+  *   log: 1,
+  *   iterations: 500,
+  *   error: 0.03,
+  *   rate: 0.05,
+  *   cost: methods.cost.CROSS_ENTROPY
+  * });
   */
   CROSS_ENTROPY: function (target, output) {
     var error = 0;
@@ -32,10 +33,19 @@ var cost = {
   /**
   * Mean Squared Error
   *
-  * @param {number} target - Ideal value
-  * @param {number[]} output - Actual values
+  * @param {number} target Ideal value
+  * @param {number[]} output Actual values
   *
-  * @returns {number} - Mean squared error
+  * @returns {number} [Mean squared error](https://medium.freecodecamp.org/machine-learning-mean-squared-error-regression-line-c7dde9a26b93)
+  *
+  * @example
+  * myNetwork.train(trainingData, {
+  *   log: 1,
+  *   iterations: 500,
+  *   error: 0.03,
+  *   rate: 0.05,
+  *   cost: methods.cost.MSE
+  * });
   */
   MSE: function (target, output) {
     var error = 0;
@@ -48,10 +58,21 @@ var cost = {
   /**
   * Binary Error
   *
-  * @param {number} target - Ideal value
-  * @param {number[]} output - Actual values
+  * @param {number} target Ideal value
+  * @param {number[]} output Actual values
   *
-  * @returns {number} misses - Times target value was missed
+  * @returns {number} misses The amount of times target value was missed
+  *
+  * @see [Hinge loss on Wikipedia](https://en.wikipedia.org/wiki/Hinge_loss)
+  *
+  * @example
+  * myNetwork.train(trainingData, {
+  *   log: 1,
+  *   iterations: 500,
+  *   error: 0.03,
+  *   rate: 0.05,
+  *   cost: methods.cost.BINARY
+  * });
   */
   BINARY: function (target, output) {
     var misses = 0;
@@ -64,10 +85,19 @@ var cost = {
   /**
   * Mean Absolute Error
   *
-  * @param {number} target - Ideal value
-  * @param {number[]} output - Actual values
+  * @param {number} target Ideal value
+  * @param {number[]} output Actual values
   *
-  * @returns {number} - Mean absoulte error
+  * @returns {number} [Mean absoulte error](https://en.wikipedia.org/wiki/Mean_absolute_error)
+  *
+  * @example
+  * myNetwork.train(trainingData, {
+  *   log: 1,
+  *   iterations: 500,
+  *   error: 0.03,
+  *   rate: 0.05,
+  *   cost: methods.cost.MAE
+  * });
   */
   MAE: function (target, output) {
     var error = 0;
@@ -80,10 +110,19 @@ var cost = {
   /**
   * Mean Absolute Percentage Error
   *
-  * @param {number} target - Ideal value
-  * @param {number[]} output - Actual values
+  * @param {number} target Ideal value
+  * @param {number[]} output Actual values
   *
-  * @returns {number} - Mean absolute percentage error
+  * @returns {number} [Mean absolute percentage error](https://en.wikipedia.org/wiki/Mean_absolute_percentage_error)
+  *
+  * @example
+  * myNetwork.train(trainingData, {
+  *   log: 1,
+  *   iterations: 500,
+  *   error: 0.03,
+  *   rate: 0.05,
+  *   cost: methods.cost.MAPE
+  * });
   */
   MAPE: function (target, output) {
     var error = 0;
@@ -96,10 +135,19 @@ var cost = {
   /**
   * Mean Squared Logarithmic Error
   *
-  * @param {number} target - Ideal value
-  * @param {number[]} output - Actual values
+  * @param {number} target Ideal value
+  * @param {number[]} output Actual values
   *
-  * @returns {number} - Mean squared logarithmic error
+  * @returns {number} - [Mean squared logarithmic error](https://peltarion.com/knowledge-center/documentation/modeling-view/build-an-ai-model/loss-functions/mean-squared-logarithmic-error)
+  *
+  * @example
+  * myNetwork.train(trainingData, {
+  *   log: 1,
+  *   iterations: 500,
+  *   error: 0.03,
+  *   rate: 0.05,
+  *   cost: methods.cost.MSLE
+  * });
   */
   MSLE: function (target, output) {
     var error = 0;
@@ -112,10 +160,19 @@ var cost = {
   /**
   * Hinge loss, for classifiers
   *
-  * @param {number} target - Ideal value
-  * @param {number[]} output - Actual values
+  * @param {number} target Ideal value
+  * @param {number[]} output Actual values
   *
-  * @returns {number} - Hinge loss
+  * @returns {number} - [Hinge loss](https://towardsdatascience.com/support-vector-machines-intuitive-understanding-part-1-3fb049df4ba1)
+  *
+  * @example
+  * myNetwork.train(trainingData, {
+  *   log: 1,
+  *   iterations: 500,
+  *   error: 0.03,
+  *   rate: 0.05,
+  *   cost: methods.cost.HINGE
+  * });
   */
   HINGE: function (target, output) {
     var error = 0;
