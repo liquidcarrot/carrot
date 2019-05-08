9d3fb0416f6302d83ef4068c9e68defd67d9610f
luiscarbonell <luiscarbonell5683@gmail.com>
2019-05-08 21:41:33 +0000

    Basic documentation for GAN

--- src/gan.js
+++ src/gan.js
@@ -1,7 +1,10 @@
 let Network = require('./architecture/network.js')
 
 /**
+ * Generative Adverserial Network (GAN)
  *
+ * @param {Array<{ inputs: Array<number>, outputs: Array<number> }>} dataset
+ * @param {number} options.growth - Ratio of fake, i.e. "generated" data, to real data.
  */
 let GAN = function(dataset, options) {
   this.generator;
