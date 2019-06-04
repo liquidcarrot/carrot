6e77b63a68a44c0ce56b785db6056dab59c5826b
luiscarbonell <luiscarbonell5683@gmail.com>
2019-06-04 01:01:47 -0400

    Added unit tests for new Neat()

--- test/units/neat.test.js
+++ test/units/neat.test.js
@@ -1,25 +1,59 @@
-let _ = require('lodash')
-let { assert, expect } = require('chai')
-let should = require('chai').should()
-let {
-  Network,
-  Neat,
-  methods,
-  config,
-  architect
-} = require('../../src/carrot')
+const _ = require("lodash");
+const { assert, expect } = require('chai');
+const should = require('chai').should();
 
+const { is, data, random } = require('../../util');
+const { Network, Neat, methods, config, architect } = require('../../src/carrot');
 
-let mutation = methods.mutation;
-
-/**
- *
- * There are 5 questions every unit test must answer.
- *
- * What is the unit under test (module, function, class, whatever)?
- * What should it do? (Prose description)
- * What was the actual output?
- * What was the expected output?
- * How do you reproduce the failure?
- *
- */
+describe("Neat", function() {
+  it("new Neat()", function() {
+    const neat = new Neat();
+    is.neat(neat);
+  })
+  it.skip("new Neat(options)", function() {
+    const options = random.options.neat();
+    const neat = new Neat(options);
+    
+    is.neat(neat);
+  })
+  it.skip("new Neat(dataset)", function() {
+    const neat = new Neat();
+    is.neat(neat);
+  })
+  it.skip("new Neat(population)", function() {
+    const neat = new Neat();
+    is.neat(neat);
+  })
+  it.skip("new Neat(input, output)", function() {
+    const neat = new Neat();
+    is.neat(neat);
+  })
+  it.skip("new Neat(dataset, options)", function() {
+    const neat = new Neat();
+    is.neat(neat);
+  })
+  it.skip("new Neat(population, options)", function() {
+    const neat = new Neat();
+    is.neat(neat);
+  })
+  it.skip("new Neat(input, output, options)", function() {
+    const neat = new Neat();
+    is.neat(neat);
+  })
+  it.skip("new Neat(population, dataset)", function() {
+    const neat = new Neat();
+    is.neat(neat);
+  })
+  it.skip("new Neat(input, output, dataset)", function() {
+    const neat = new Neat();
+    is.neat(neat);
+  })
+  it.skip("new Neat(population, dataset, options)", function() {
+    const neat = new Neat();
+    is.neat(neat);
+  })
+  it.skip("new Neat(input, output, dataset, options)", function() {
+    const neat = new Neat();
+    is.neat(neat);
+  })
+})
