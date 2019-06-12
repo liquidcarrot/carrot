176bc647de8faa8ead5f8b0736c5d685b6cd7859
Luis Carbonell <luiscarbonell5683@gmail.com>
2019-06-11 23:14:05 -0400

    Removed stub test; Style-guide test messages

--- test/units/architecture/architect.test.js
+++ test/units/architecture/architect.test.js
@@ -1,34 +1,15 @@
-let { assert, expect } = require('chai');
-let should = require('chai').should();
-let {architect, Network, Node } = require('../../../src/carrot');
+const { assert, expect } = require('chai');
+const should = require('chai').should();
+const {architect, Network, Node } = require('../../../src/carrot');
 
-/**
- *
- * There are 5 questions every unit test must answer.
- *
- * What is the unit under test (module,fu nction, class, whatever)?
- * What should it do? (Prose description)
- * What was the actual output?
- * What was the expected output?
- * How do you reproduce the failure?
- *
- */
 describe('architect', function(){
-  describe('Perceptron', function() {
-    it('given input: 0, hidden: 0, output: 0, should throw Error', function(){
-           assert.throws(function() {  return new architect.Perceptron(0, 0, 0)  }, RangeError);
-       });
-  
-       it('should return false if invalid user id', function(){
-           //Input: 5 neurons, 2 Hidden Layers: 7 neurons, Output: 2 neurons
-           let example = new architect.Perceptron(5, 7, 7, 2);
-       });
+  describe('achitect.Perceptron()', function() {
+    it('architect.Perceptron(input=0, hidden=0, output=0) => {RangeError}', function(){
+      expect(new architect.Perceptron(0, 0, 0)).to.throw(RangeError);
+    });
 
-       it('given three argument should return network', function(){
-           //Input: three neurons, 0 Hidden Layers. Output: one neuron
-           expect(new architect.Perceptron(3, 0, 1)).to.be.an.instanceOf(Network);
-       });
-
-    
+    it('architect.Perceptron(input=3, hidden=0, output=1) => {Network}', function(){
+      expect(new architect.Perceptron(3, 0, 1)).to.be.an.instanceOf(Network);
+    });
   });
 });
