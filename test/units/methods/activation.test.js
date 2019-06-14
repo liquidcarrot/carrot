767928e36694683fbf15506f7497b7e165ef35ab
IviieMtz <iimartinezh@miners.utep.edu>
2019-06-13 20:21:01 -0600

    Test TANH and Activation TANH init x

--- test/units/methods/activation.test.js
+++ test/units/methods/activation.test.js
@@ -24,11 +24,11 @@ it("activation.TANH() => {ReferenceError}", function(){
     expect(() => activation.TANH()).to.throw(ReferenceError);    
 })
 it("activation.TANH(number, derivative=false) => {number}", function(){
-    x=_.random(0,50,true);  
+    let x=_.random(0,50,true);  
     expect( activation.TANH(x, false)).to.equal(Math.tanh(x));
 })
 it("activation.TANH(number, derivative=true) => {number}", function(){
-    x=_.random(0,50,true);
+    let x=_.random(0,50,true);
     expect(activation.TANH(x, true)).to.equal(1 - Math.pow(Math.tanh(x), 2));
 })
 it("activation.TANH(undefined,derivative=true) => {ReferenceError}", function(){
@@ -37,13 +37,13 @@ it("activation.TANH(undefined,derivative=true) => {ReferenceError}", function(){
 //Array
 it("activation.TANH(numbers, derivative=false) => {numbers}", function(){
     let z=[];
-    x=Array.from({length: _.random(1,4)}, () => _.random(2,50,true));  
+    let x=Array.from({length: _.random(1,4)}, () => _.random(2,50,true));  
     for(let i=0;i<x.length;i++) {z[i]=Math.tanh(x[i]); }
     expect(activation.TANH(x, false)).to.eql(z);
 })
 it("activation.TANH(numbers, derivative=true) => {numbers}", function(){
     let z=[];
-    x=Array.from({length: _.random(1,5)}, () => _.random(2,50,true));  
+    let x=Array.from({length: _.random(1,5)}, () => _.random(2,50,true));  
     for(let i=0;i<x.length;i++) {z[i]= 1 - Math.pow(Math.tanh(x[i]), 2); }
     expect(activation.TANH(x, true)).to.eql(z);
 })
