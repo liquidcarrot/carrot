7b07e71e716bacf5bed5e103365bc7742a1b532e
luiscarbonell <luiscarbonell5683@gmail.com>
2019-06-22 19:24:48 -0400

    Added Unit Tests for Array Properties of Mutation.js

--- test/units/methods/mutation.test.js
+++ test/units/methods/mutation.test.js
@@ -5,12 +5,20 @@ const { mutation } = methods;
 describe("gating", function() {
   Object.keys(mutation).forEach(function(type) {
     describe(`mutation.${type}`, function() {
-      it(`mutation.${type} => {Object}`, function() {
-        expect(mutation[type]).to.be.an("object");
-      })
-      it(`mutation.${type}.name => {string}`, function() {
-        expect(mutation[type].name).to.be.a("string");
-      })
+      if(type === "ALL" || type === "FFW") {
+        it(`mutation.${type} => {Array}`, function() {
+          expect(mutation[type]).to.be.an("array");
+          expect(mutation[type]).to.be.an.instanceof(Array);
+        })
+      }
+      else {
+        it(`mutation.${type} => {Object}`, function() {
+          expect(mutation[type]).to.be.an("object");
+        })
+        it(`mutation.${type}.name => {string}`, function() {
+          expect(mutation[type].name).to.be.a("string");
+        })
+      }
     })
   })
 })
