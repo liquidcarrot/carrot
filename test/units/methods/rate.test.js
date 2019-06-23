a0b71f7d83f3fdfcdd1898dbcf32cf157fea8f6f
luiscarbonell <luiscarbonell5683@gmail.com>
2019-06-23 16:48:12 -0400

    Refactored methods.rate; Added Unit Test for methods.rate

--- test/units/methods/rate.test.js
+++ test/units/methods/rate.test.js
@@ -1,16 +1,30 @@
-let { assert, expect } = require('chai');
-let should = require('chai').should();
-let carrot = require('../../../src/carrot');
+const { expect } = require('chai');
+const { methods } = require('../../../src/carrot');
+const { rate } = methods;
 
-/**
- *
- * There are 5 questions every unit test must answer.
- *
- * What is the unit under test (module, function, class, whatever)?
- * What should it do? (Prose description)
- * What was the actual output?
- * What was the expected output?
- * How do you reproduce the failure?
- *
- */
- 
+describe("rate", function() {
+  Object.keys(rate).forEach(function(type) {
+    describe(`rate.${type}()`, function() {
+      const base_rate = Math.random();
+      const iteration = Math.round(Math.random() * 100);
+      const options = {
+        gamma: Math.random(),
+        power: Math.ceil(Math.random() * 5),
+        step_size: Math.ceil(Math.random() * 100)
+      }
+      
+      // @returns {ReferenceError}
+      it(`rate.${type}() => {ReferenceError}`, function() {
+        expect(() => rate[type]()).to.throw(ReferenceError);
+      })
+      
+      // @returns {number}
+      it(`rate.${type}(base_rate=${base_rate}, iteration=${iteration}) => {number}`, function() {
+        expect(rate[type](base_rate, iteration)).to.be.a("number");
+      })
+      it(`rate.${type}(base_rate=${base_rate}, iteration=${iteration}, options=${options}) => {number}`, function() {
+        expect(rate[type](base_rate, iteration, options)).to.be.a("number");
+      })
+    })
+  })
+})
