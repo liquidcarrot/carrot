82795dc0236776bd8092620b4c7d8e98ec889703
luiscarbonell <luiscarbonell5683@gmail.com>
2019-07-03 23:35:58 -0400

    Added Population.js to replace Neat.js

--- src/carrot.js
+++ src/carrot.js
@@ -10,6 +10,7 @@ const Carrot = {
   Layer: require('./architecture/layer'),
   Node: require('./architecture/node'),
   Neat: require('./neat'),
+  Population: require('./architecture/population'),
   GAN: require('./gan'),
   multi: require('./multithreading/multi')
 };
