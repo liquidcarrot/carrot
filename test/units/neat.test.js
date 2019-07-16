06948514bd312ddb4720d6f0ce1a5a6e195c7683
luiscarbonell <luiscarbonell5683@gmail.com>
2019-07-16 19:58:30 -0400

    Added NEAT.createPopulation() Unit Tests

--- test/units/neat.test.js
+++ test/units/neat.test.js
@@ -7,19 +7,161 @@ const { Network, Neat, methods, config, architect } = require('../../src/carrot'
 
 describe("Neat", function() {
   describe("new Neat()", function() {
+    it("new Neat()", function() {
+      const neat = new Neat();
+      is.neat(neat);
+    })
+    it("new Neat(options)", function() {
+      const options = random.options.neat();
+      const neat = new Neat(options);
+      
+      is.neat(neat);
+      has.options(neat, options);
+    })
+    it("new Neat(dataset)", function() {
+      const dataset = data.XNOR;
+      const neat = new Neat(dataset);
+      
+      is.neat(neat);
+      has.dataset(neat, dataset);
+    })
+    it("new Neat(input, output)", function() {
+      const inputs = _.random(50);
+      const outputs = _.random(50)
+      const neat = new Neat(inputs, outputs);
+      
+      is.neat(neat);
+      has.dimensions(neat, inputs, outputs);
+    })
+    it("new Neat(dataset, options)", function() {
+      const dataset = data.XNOR;
+      const options = random.options.neat();
+      
+      const neat = new Neat(dataset, options);
+      
+      is.neat(neat);
+      has.dataset(neat, dataset);
+      has.options(neat, options);
+      
+    })
+    it("new Neat(input, output, options)", function() {
+      const inputs = _.random(50);
+      const outputs = _.random(50);
+      const options = random.options.neat();
+      
+      const neat = new Neat(inputs, outputs, options);
+      
+      is.neat(neat);
+      has.options(neat, options);
+      has.dimensions(neat, inputs, outputs);
+    })
+    it("new Neat(input, output, dataset)", function() {
+      const inputs = _.random(50);
+      const outputs = _.random(50);
+      const dataset = data.XNOR;
+      
+      const neat = new Neat(inputs, outputs, dataset);
+      
+      is.neat(neat);
+      has.dataset(neat, dataset);
+      has.dimensions(neat, inputs, outputs);
+    })
+    it("new Neat(input, output, dataset, options)", function() {
+      const inputs = _.random(50);
+      const outputs = _.random(50);
+      const dataset = data.XNOR;
+      const options = random.options.neat();
+      
+      const neat = new Neat(inputs, outputs, dataset, options);
+      
+      is.neat(neat);
+      has.dataset(neat, dataset);
+      has.options(neat, options);
+      has.dimensions(neat, inputs, outputs);
+    })
     
+    // it.skip("new Neat(population)", function() {
+    //   const neat = new Neat();
+    //   is.neat(neat);
+    // })
+    // it.skip("new Neat(population, options)", function() {
+    //   const neat = new Neat();
+    //   is.neat(neat);
+    // })
+    // it.skip("new Neat(population, dataset)", function() {
+    //   const neat = new Neat();
+    //   is.neat(neat);
+    // })
+    // it.skip("new Neat(population, dataset, options)", function() {
+    //   const neat = new Neat();
+    //   is.neat(neat);
+    // })
   })
   describe("neat.createPool()", function() {
-    it("neat.createPool() => {Network[]}")
-    it("neat.createPool(network) => {Network[]}")
-    it("neat.createPool(size) => {Network[]}")
-    it("neat.createPool(network, size) => {Network[]}")
+    // Should ignore for `neat.createPopulation()`...
   })
   describe("neat.createPopulation()", function() {
-    it("neat.createPopulation() => {Network[]}")
-    it("neat.createPopulation(network) => {Network[]}")
-    it("neat.createPopulation(size) => {Network[]}")
-    it("neat.createPopulation(network, size) => {Network[]}")
+    it("neat.createPopulation() => {Network[]}", function() {
+      const neat = new Neat();
+      const population = neat.createPopulation();
+      
+      expect(population).to.be.an.instanceof(Array);
+      expect(population).to.have.lengthOf.above(0);
+      
+      for (let genome = 0; genome < population.length; genome++) {
+        expect(population[genome]).to.be.an.instanceOf(Network);
+      }
+    })
+    it("neat.createPopulation(network) => {Network[]}", function() {
+      const inputs = Math.ceil(Math.random() * 10);
+      const outputs = Math.ceil(Math.random() * 10);
+      const network = new Network(inputs, outputs);
+      
+      const neat = new Neat();
+      
+      const population = neat.createPopulation(network);
+      
+      expect(population).to.be.an.instanceof(Array);
+      expect(population).to.have.lengthOf.above(0);
+      
+      for (let genome = 0; genome < population.length; genome++) {
+        expect(population[genome]).to.be.an.instanceOf(Network);
+        expect(population[genome].input_size).to.equal(inputs);
+        expect(population[genome].output_size).to.equal(outputs);
+      }
+    })
+    it("neat.createPopulation(size) => {Network[]}", function() {
+      const size = Math.ceil(Math.random() * 100);
+      
+      const neat = new Neat();
+      const population = neat.createPopulation(size);
+      
+      expect(population).to.be.an.instanceof(Array);
+      expect(population).to.have.lengthOf(size);
+      
+      for (let genome = 0; genome < population.length; genome++) {
+        expect(population[genome]).to.be.an.instanceOf(Network);
+      }
+    })
+    it("neat.createPopulation(network, size) => {Network[]}", function() {
+      const size = Math.ceil(Math.random() * 100);
+      
+      const inputs = Math.ceil(Math.random() * 10);
+      const outputs = Math.ceil(Math.random() * 10);
+      const network = new Network(inputs, outputs);
+      
+      const neat = new Neat();
+      const population = neat.createPopulation(network, size);
+      
+      expect(population).to.be.an.instanceof(Array);
+      expect(population).to.have.lengthOf(size);
+      
+      for (let genome = 0; genome < population.length; genome++) {
+        expect(population[genome]).to.be.an.instanceOf(Network);
+        expect(population[genome].input_size).to.equal(inputs);
+        expect(population[genome].output_size).to.equal(outputs);
+      }
+    })
   })
   describe("neat.replace()", function() {
     it("neat.replace() => {ReferenceError}")
@@ -93,94 +235,5 @@ describe("Neat", function() {
   
   
   
-  it("new Neat()", function() {
-    const neat = new Neat();
-    is.neat(neat);
-  })
-  it("new Neat(options)", function() {
-    const options = random.options.neat();
-    const neat = new Neat(options);
-    
-    is.neat(neat);
-    has.options(neat, options);
-  })
-  it("new Neat(dataset)", function() {
-    const dataset = data.XNOR;
-    const neat = new Neat(dataset);
-    
-    is.neat(neat);
-    has.dataset(neat, dataset);
-  })
-  it("new Neat(input, output)", function() {
-    const inputs = _.random(50);
-    const outputs = _.random(50)
-    const neat = new Neat(inputs, outputs);
-    
-    is.neat(neat);
-    has.dimensions(neat, inputs, outputs);
-  })
-  it("new Neat(dataset, options)", function() {
-    const dataset = data.XNOR;
-    const options = random.options.neat();
-    
-    const neat = new Neat(dataset, options);
-    
-    is.neat(neat);
-    has.dataset(neat, dataset);
-    has.options(neat, options);
-    
-  })
-  it("new Neat(input, output, options)", function() {
-    const inputs = _.random(50);
-    const outputs = _.random(50);
-    const options = random.options.neat();
-    
-    const neat = new Neat(inputs, outputs, options);
-    
-    is.neat(neat);
-    has.options(neat, options);
-    has.dimensions(neat, inputs, outputs);
-  })
-  it("new Neat(input, output, dataset)", function() {
-    const inputs = _.random(50);
-    const outputs = _.random(50);
-    const dataset = data.XNOR;
-    
-    const neat = new Neat(inputs, outputs, dataset);
-    
-    is.neat(neat);
-    has.dataset(neat, dataset);
-    has.dimensions(neat, inputs, outputs);
-  })
-  it("new Neat(input, output, dataset, options)", function() {
-    const inputs = _.random(50);
-    const outputs = _.random(50);
-    const dataset = data.XNOR;
-    const options = random.options.neat();
-    
-    const neat = new Neat(inputs, outputs, dataset, options);
-    
-    is.neat(neat);
-    has.dataset(neat, dataset);
-    has.options(neat, options);
-    has.dimensions(neat, inputs, outputs);
-  })
   
-  
-  it.skip("new Neat(population)", function() {
-    const neat = new Neat();
-    is.neat(neat);
-  })
-  it.skip("new Neat(population, options)", function() {
-    const neat = new Neat();
-    is.neat(neat);
-  })
-  it.skip("new Neat(population, dataset)", function() {
-    const neat = new Neat();
-    is.neat(neat);
-  })
-  it.skip("new Neat(population, dataset, options)", function() {
-    const neat = new Neat();
-    is.neat(neat);
-  })
 })
