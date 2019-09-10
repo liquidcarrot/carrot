d5508bf64523b404dd747c73e118baa765090fd8
Christian George Echevarria-Reina <christian.george.echevarria@gmail.com>
2019-09-09 22:23:44 -0400

    Fix Webpack build bug

--- webpack.config.js
+++ webpack.config.js
@@ -10,16 +10,43 @@ const variants = require("parallel-webpack").createVariants;
 /* Read license */
 const license = fs.readFileSync('./LICENSE', 'utf-8');
 
+// basically a copy of Webpack 4 production defaults, minus anything that interferes with an unminified build
 function config(options) {
   return {
     "entry": "./src/carrot.js",
+    "performance": {
+      "hints": "warning"
+    },
     "output": {
+      "pathinfo": false,
       "path": path.resolve(__dirname, "dist"),
       "filename": `carrot.${options.target}${options.mode === "development" ? "" : ".min"}.js`,
       "library": "Carrot",
-      "libraryTarget": options.target,
-			"plugins": [new webpack.BannerPlugin(license)],
+      "libraryTarget": options.target
     },
+    "plugins": [
+      new webpack.BannerPlugin(license),
+      new webpack.optimize.ModuleConcatenationPlugin()
+  ],
+    "optimization": {
+      "namedModules": false,
+      "namedChunks": false,
+      "flagIncludedChunks": true,
+      "occurrenceOrder": true,
+      "sideEffects": true,
+      "usedExports": true,
+      "concatenateModules": true,
+      "splitChunks": {
+        "hidePathInfo": true,
+        "minSize": 30000,
+        "maxAsyncRequests": 5,
+        "maxInitialRequests": 3
+      },
+      "noEmitOnErrors": true,
+      "checkWasmTypes": true,
+      "minimize": (options.mode === "development") ? false : true
+    },
+    "mode": "none" // avoid development / production defaults
   }
 }
 
