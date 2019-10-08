9b1b8f9f7fa34037af17d4657c33a1eede589963
luiscarbonell5683 <luiscarbonell5683@penguin>
2019-10-07 20:45:36 -0400

    Added GPU Kernel Test 1

--- gpu/index.js
+++ gpu/index.js
@@ -1,3 +1,89 @@
+const { GPU }  = require("gpu.js");
+
+const gpu = new GPU();
+
+const SIZE = 512;
+
+const A = Array.from({
+  length: 512
+}, function() {
+  return Array.from({
+    length: 512
+  }, function() {
+    return Math.random() * 2 - 1;
+  });
+});
+
+const B = Array.from({
+  length: 512
+}, function() {
+  return Array.from({
+    length: 512
+  }, function() {
+    return Math.random() * 2 - 1;
+  });
+});
+
+console.log(A);
+console.log(B);
+
+const dotProduct = gpu.createKernel(function(a, b) {
+  let sum = 0;
+  for(let t = 0; t < 512; t++) {
+    sum += a[this.thread.x][t] * b[t][this.thread.x];
+  }
+  return sum;
+}).setOutput([512, 512]);
+
+console.log(dotProduct);
+
+const C = dotProduct(A, B);
+
+console.log(C);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // GPU Acceleration:
 // - OpenGL
 // - WebGL
