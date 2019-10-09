a9f19599486620b3eace3f79c9a80a79d836aed4
luiscarbonell5683 <luiscarbonell5683@penguin>
2019-10-09 16:43:07 -0400

    Added connections type

--- gpu/activations.js
+++ gpu/activations.js
@@ -1,73 +1,21 @@
+// Different connection kernels
 const GPU  = require("./gpu.js");
 
-// Sigmoidal GPU Kernel
-{
-  // 512 Weights
-  const weights = Array.from({
-    length: 2
-  }, function() {
-    return Math.random() * 2 - 1; // -1 < x < 1
-  });
-  // 512 Numbers
-  const results = Array.from({
-    length: 2
-  }, function() {
-    return Math.random() * 200 - 100; // -100 < x < 100
-  });
+// GPU Kernel: Weighted Sum Function
+// Takes two Nx1 arrays
+// Performs `sum([N, N, ...], [N, N, ...])`, where `N` is a natural number
+const weighted = gpu.createKernel(function(W, X) {
+  // w * x
+  return W[this.thread.x] * X[this.thread.x];
+}).setOutput([512]);
 
-  console.log(weights.length);
-  console.log(results.length);
+// GPU Kernel: Cyclical Sum Function
+// Take two Nx1 arrays
+// Performs `sum(N * sin(N * N - N)`, where `N` is a natural number
+const cyclical = gpu.createKernel(function(W, X) {
+  // a * sin(b * x - c)
+  return W[this.thread.x][0] * Math.sin(W[this.thread.x][1] * X[this.thread.x] - W[this.thread.x][2]);
+}.setOutput([512]);
 
-  console.log(weights);
-  console.log(results);
-
-  // GPU Kernel: Sigmoidal Function
-  const sigmoid = gpu.createKernel(function(W, X) {
-    return W[this.thread.x] * X[this.thread.x];
-  }).setOutput([2]);
-
-  const output = sigmoid(weights, results);
-
-  console.log(output);
-}
-
-// Basic GPU Kernel
-if(false) {
-  const A = Array.from({
-    length: 512
-  }, function() {
-    return Array.from({
-      length: 512
-    }, function() {
-      return Math.random() * 2 - 1;
-    });
-  });
-
-  const B = Array.from({
-    length: 512
-  }, function() {
-    return Array.from({
-      length: 512
-    }, function() {
-      return Math.random() * 2 - 1;
-    });
-  });
-
-  console.log(A);
-  console.log(B);
-
-
-  const dotProduct = gpu.createKernel(function(a, b) {
-    let sum = 0;
-    for(let t = 0; t < 512; t++) {
-      sum += a[this.thread.x][t] * b[t][this.thread.x];
-    }
-    return sum;
-  }).setOutput([512, 512]);
-
-  console.log(dotProduct);
-
-  const C = dotProduct(A, B);
-
-  console.log(C);
-}
+// [weighted, cyclical]
+module.exports = [weighted, cyclical];
