617a4bec4f7a1f9b2942a38e2a30f5add4cd58c0
luiscarbonell <luiscarbonell5683@gmail.com>
2019-10-09 20:12:36 -0400

    Added basic state to network

--- gpu/network.js
+++ gpu/network.js
@@ -1,5 +1,7 @@
 function Network() {
-
+  this.connections = []; // [0, [w], [a, b, c], ...]
+  this.state = []; // f, f', e, e', o
+  this.methods = []; // F, F', E, E', O
 }
 
 module.exports = Network;
