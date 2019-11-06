64f17b84a0c53561ac52da828edc8aa08196efb0
Manuel Raimann <raimannma@outlook.de>
2019-11-06 15:46:13 +0100

    removing ability to remove lower values.
    Will be replaced by PER in future.

--- src/util/window.js
+++ src/util/window.js
@@ -2,24 +2,23 @@ Array.min = function (array) {
   return Math.min.apply(Math, array);
 };
 
-function Window(size, removeLowestValues) {
+function Window(size) {
   this.v = [];
-  this.removeLowestValues = removeLowestValues;
   this.size = typeof (size) === 'undefined' ? 100 : size;
   this.sum = 0;
 }
 
 Window.prototype = {
-  add: function (x) {
-    this.v.push(x);
-    this.sum += x;
-    if (this.v.length > this.size) {
-      if (this.removeLowestValues) {
-        this.v.sort(function (a, b) {
-          return Math.abs(a[a.length - 1]) - Math.abs(b[b.length - 1]);
-        });
+  add: function(elem) {
+    this.v.push(elem);
+    if (!isNaN(elem)) {
+      this.sum += elem;
+      if (this.v.length > this.size) {
+        this.sum -= this.v.shift();
       }
-      this.sum -= this.v.shift();
+    }
+    if (this.v.length > this.size) {
+      this.v.shift();
     }
   },
   getAverage: function () {
