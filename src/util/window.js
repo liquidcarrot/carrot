c90b0d0813f132aa805ca880e65e3aff087d3526
Manuel Raimann <raimannma@outlook.de>
2019-11-06 16:14:15 +0100

    editing doc of Window marking Window as deprecated. should be replaced by an replay buffer

--- src/util/window.js
+++ src/util/window.js
@@ -1,19 +1,43 @@
+/**
+ * A Window is just an array with an fixed max-size.
+ * It deletes old entries, to create space for new ones.
+ *
+ * @param size maximum size
+ *
+ * TODO: Should be replaced with a replay buffer
+ * @deprecated
+ * @constructor
+ */
 function Window(size) {
   this.v = [];
   this.size = typeof (size) === 'undefined' ? 100 : size;
 }
 
 Window.prototype = {
+  /**
+   * Adds an element to the array, and if max-size is reached the first element will be deleted.
+   * @param elem new element which gets added
+   */
   add: function(elem) {
     this.v.push(elem);
     if (this.v.length > this.size) {
       this.v.shift();
     }
   },
+
+  /**
+   * This method returns a random entry from the array.
+   *
+   * @returns {*}
+   */
   pickRandom: function () {
     let x = this.v[Math.floor(Math.random() * this.v.length)];
     return x === undefined ? this.v[0] : x;
   },
+
+  /**
+   * This method resets the array.
+   */
   reset: function () {
     this.v = [];
   }
