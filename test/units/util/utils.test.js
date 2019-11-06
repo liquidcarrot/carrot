651c6bebf4af4c7b61af7e882fbd7a2e0686a5ae
Manuel Raimann <raimannma@outlook.de>
2019-11-06 19:00:24 +0100

    refactor test method

--- test/units/util/utils.test.js
+++ test/units/util/utils.test.js
@@ -4,13 +4,13 @@ const Utils = require('../../../src/util/utils');
 describe('Utils', function() {
   it('getMaxValueIndex', function() {
     for (let testIteration = 0; testIteration < 100; testIteration++) {
-      let randomArray = [];
+      let arr = [];
       for (let i = 0; i <= 100; i++) {
-        randomArray[i] = i;
+        arr[i] = i;
       }
 
-      let randomArrayShuffled = Utils.shuffle(randomArray);
-      expect(randomArrayShuffled[Utils.getMaxValueIndex(randomArrayShuffled)]).equal(100);
+      let shuffled = Utils.shuffle(arr);
+      expect(shuffled[Utils.getMaxValueIndex(shuffled)]).equal(100);
     }
   });
 });
