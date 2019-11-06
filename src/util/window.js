d48cab2c13cced5e14ce9e5dca80293c3a77f90a
Manuel Raimann <raimannma@outlook.de>
2019-11-06 15:55:08 +0100

    removing the getAverage function

--- src/util/window.js
+++ src/util/window.js
@@ -1,28 +1,14 @@
-Array.min = function (array) {
-  return Math.min.apply(Math, array);
-};
-
 function Window(size) {
   this.v = [];
   this.size = typeof (size) === 'undefined' ? 100 : size;
-  this.sum = 0;
 }
 
 Window.prototype = {
   add: function(elem) {
     this.v.push(elem);
-    if (!isNaN(elem)) {
-      this.sum += elem;
-      if (this.v.length > this.size) {
-        this.sum -= this.v.shift();
-      }
-    }
     if (this.v.length > this.size) {
       this.v.shift();
     }
-  },
-  getAverage: function () {
-    return this.sum / this.v.length;
   },
   pickRandom: function () {
     let x = this.v[Math.floor(Math.random() * this.v.length)];
@@ -30,7 +16,6 @@ Window.prototype = {
   },
   reset: function () {
     this.v = [];
-    this.sum = 0;
   }
 };
 
