76e4174d7614b261913d263a86c234994bfda81b
M. Raimann <raimannma@outlook.de>
2019-11-14 20:28:21 +0100

    changing timeout for DDPG object creation test method
    for slower PCs

--- test/units/architecture/rl/ddpg.test.js
+++ test/units/architecture/rl/ddpg.test.js
@@ -89,6 +89,7 @@ function testLearning(agent) {
 
 describe('DDPG', function() {
   it('Object creation', function() {
+    this.timeout(10000);
     for (let i = 0; i < 10; i++) {
       let numStates = Math.floor(Math.random() * 100 + 1);
       let numActions = Math.floor(Math.random() * 100 + 1);
