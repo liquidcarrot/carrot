3e4ccea944805c605c964e6f9e86ab42c6958da9
Christian George Echevarria-Reina <christian.george.echevarria@gmail.com>
2019-12-02 08:41:13 -0500

    Add tests for util.manageNeatId

--- test/units/util/utils.test.js
+++ test/units/util/utils.test.js
@@ -69,28 +69,42 @@ describe("src/util/utils.js", function () {
     })
   })
   describe('util.manageNeatId', function() {
-    it("() => TypeError", function() {
-      expect(() => util.cantorLookup()).to.throw(TypeError);
+    it("({ id: 0 }, { id: 0 }, { 0: 22, last: x }) => 22 | returns known id when same pair is given", function() {
+      let response = util.manageNeatId({ id: 0 }, { id: 0 }, { 0: 22, last: 0 }) // cantor pair for 0,0 is zero, use as key to retrieve value
+      expect(response).equal(22)
+      // one more time for rigor
+      response = util.manageNeatId({ id: 0 }, { id: 0 }, { 0: 22, last: 0 }) // cantor pair for 0,0 is zero, use as key to retrieve value
+      expect(response).equal(22)
     })
 
-    it("(number, number, referenceObject) => {ReferenceError} | errors when 'from' & 'to' are not objects with '.id' properties", function() {
-      expect(() => util.cantorLookup(1,1, { last: 0 })).to.throw(ReferenceError);
+    it("({ id: 0 }, { id: 0 }, { 0: 22, last: x }) => 22 | preserves reference object when same pair is given", function() {
+      const connIds = { 0: 22, last: 22 } // 0 is the only key entry
+      let response = util.manageNeatId({ id: 0 }, { id: 0 }, connIds) // cantor pair for 0,0 is zero, use as key to retrieve value
+      expect(connIds).eql({ 0: 22, last: 22 })
+      // one more time for rigor
+      response = util.manageNeatId({ id: 0 }, { id: 0 }, connIds) // cantor pair for 0,0 is zero, use as key to retrieve value
+      expect(connIds).eql({ 0: 22, last: 22 })
     })
 
-    it("({ id }, { id }, null) => {TypeError} | errors when neat id reference object is missing", function() {
-      expect(() => util.cantorLookup({ id: 976 }, { id: 391 })).to.throw(TypeError)
-    })
+    it('({ id: 0 }, { id: 1 }, { 0: 22, last: 22 }) => 23 | returns .last + 1 when new entry is received', function () {
+      const connIds = { 0: 22, last: 22 } // 0 is the only key entry
+      const response = util.manageNeatId({ id: 0 }, { id: 1 }, connIds) // (0,1) cantor pair is 2
 
-    it("({ id: 0 }, { id: 0 }, { 0: 22, last: x }) => { key: 0, id: 22 } | response is an object with correct key and id value", function() {
-      const response = util.cantorLookup({ id: 0 }, { id: 0 }, { 0: 22, last: 0 }) // cantor pair for 0,0 is zero, use as key to retrieve value
-      expect(response).a('object')
-      expect(response.key).equal(0)
-      expect(response.id).equal(22)
-    })
+      expect(response).equal(23); // should be one plus the last known, for clarity this matches the 0 key's value plus one
+    });
 
-    it("({ id }, { id }, { last: x }) => { key: x, id: undefined } | returns id: undefined when no entry present in object", function() {
-      const response = util.cantorLookup({ id: 0 }, { id: 0 }, { last: 0 })
-      expect(response.id).equal(undefined)
-    })
+    it('({ id: 0 }, { id: 1 }, { 0: 22, last: 22 }) | mutates reference object when (from, to) pair unknown to reference object is received', function () {
+      const connIds = { 0: 22, last: 22 } // 0 is the only key entry
+      const response = util.manageNeatId({ id: 0 }, { id: 1 }, connIds) // (0,1) cantor pair is 2
+
+      expect(connIds).not.eql({ 0: 22, last: 22 }); // should not be deeply equal initial object i.e. values should have changed
+    });
+
+    it('({ id: 0 }, { id: 1 }, { 0: 22, last: 22 }) => 23 | updates .last when new entry is received', function () {
+      const connIds = { 0: 22, last: 22 } // 0 is the only key entry
+      const response = util.manageNeatId({ id: 0 }, { id: 1 }, connIds) // (0,1) cantor pair is 2
+
+      expect(connIds.last).equal(23); // should be one plus the last known, for clarity this matches the 0 key's value plus one
+    });
   })
 })
