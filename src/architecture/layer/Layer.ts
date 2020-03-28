d10c97a17708efda3d997c94ff0e4cdfb5381d7e
M. Raimann <raimannma@outlook.de>
2020-03-28 12:30:50 +0100

    adding an abstract Layer  super class

---
+++ src/architecture/layer/Layer.ts
@@ -0,0 +1,17 @@
+import {Connection} from "../Connection";
+import {Node} from "../Node";
+
+export abstract class Layer {
+    public inputSize: number;
+    public outputSize: number;
+    public nodes: Node[];
+    public connections: Connection[];
+
+
+    protected constructor(inputSize: number, outputSize: number) {
+        this.inputSize = inputSize;
+        this.outputSize = outputSize;
+        this.nodes = [];
+        this.connections = [];
+    }
+}
