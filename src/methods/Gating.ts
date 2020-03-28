423eac26008d4a6d9b480fedb473d62401b3f8d7
M. Raimann <raimannma@outlook.de>
2020-03-28 11:16:56 +0100

    move from gating.js to Gating.ts

---
+++ src/methods/Gating.ts
@@ -0,0 +1,39 @@
+/**
+ * Specifies how to gate a connection between two groups of multiple neurons.
+ * Gating makes the weights in networks more dynamic by adapting them to their gating node. Read more about it [here](https://en.wikipedia.org/wiki/Synaptic_gating). For specific implementations of gating, see [Node](Node), [Group](Group) and [Network](Network)
+ *
+ * NOT YET IMPLEMENTED
+ * @namespace
+ */
+export enum Gating {
+    /**
+     * @constant
+     * @type {object}
+     * @description Every node in the gating group will gate (at least) 1 node in the emitting group and all its connections to the other, receiving group
+     * @default
+     *
+     * @example
+     * let example = "TODO"
+     */
+    OUTPUT,
+    /**
+     * @constant
+     * @type {object}
+     * @description Every node in the gating group will gate (at least) 1 node in the receiving group and all its connections from the other, emitting group
+     * @default
+     *
+     * @example
+     * let example = "TODO"
+     */
+    INPUT,
+    /**
+     * @constant
+     * @type {object}
+     * @description Every node in the gating group will gate (at least) 1 self connection in the emitting/receiving group
+     * @default
+     *
+     * @example
+     * let example = "TODO"
+     */
+    SELF,
+}
