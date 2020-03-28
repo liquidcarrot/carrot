dffdb7a9df57aa3ac24721ea67d55bb1d881aac1
M. Raimann <raimannma@outlook.de>
2020-03-28 12:32:14 +0100

    Move from architect.js JavaScript to Architect.ts TypeScript

---
+++ src/architecture/Architect.ts
@@ -0,0 +1,17 @@
+import {Layer} from "./layer/Layer";
+
+export class Architect {
+    private layers: Layer[];
+
+    constructor() {
+        this.layers = [];
+    }
+
+    public addLayer(layer: Layer): Architect {
+        if (this.layers[this.layers.length - 1].outputSize !== layer.inputSize) {
+            throw new RangeError("Output size of last layer is unequal input size of this layer! " + this.layers[this.layers.length - 1].outputSize + " -> " + layer.inputSize);
+        }
+        this.layers.push(layer);
+        return this;
+    }
+}
