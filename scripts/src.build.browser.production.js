a982064c5e177d731295b1c50b605a45d227469a
M. Raimann <raimannma@outlook.de>
2020-03-30 11:32:53 +0200

    remove webpack run eslint built dist

--- scripts/src.build.browser.production.js
+++ scripts/src.build.browser.production.js
@@ -6,17 +6,17 @@ const entry = path.resolve(__dirname, '../build/src/index.js');
 
 // Parcel.js - Browser Production Options
 const options = {
-    outDir: path.resolve(__dirname, '../dist/production'),
-    outFile: path.resolve(__dirname, '../dist/production/index.browser.min.js'),
-    minify: true,
-    target: 'browser',
-    bundleNodeModules: true,
-    global: 'carrot',
+  outDir: path.resolve(__dirname, '../dist/production'),
+  outFile: path.resolve(__dirname, '../dist/production/index.browser.min.js'),
+  minify: true,
+  target: 'browser',
+  bundleNodeModules: true,
+  global: 'carrot',
 };
 
 // Parcel.js - Bundle Source Code for Node
 const bundler = new Bundler(entry, options);
-const bundle = bundler.bundle();
+bundler.bundle();
 
 // Closes process when build ends
 bundler.on('buildEnd', () => process.exit());
--- scripts/src.build.node.production.js
+++ scripts/src.build.node.production.js
@@ -6,16 +6,16 @@ const entry = path.resolve(__dirname, '../build/src/index.js');
 
 // Parcel.js - Node Production Options
 const options = {
-    outDir: path.resolve(__dirname, '../dist/production'),
-    outFile: path.resolve(__dirname, '../dist/production/index.min.js'),
-    minify: true,
-    scopeHoist: true,
-    global: 'carrot',
+  outDir: path.resolve(__dirname, '../dist/production'),
+  outFile: path.resolve(__dirname, '../dist/production/index.min.js'),
+  minify: true,
+  scopeHoist: true,
+  global: 'carrot',
 };
 
 // Parcel.js - Bundle Source Code for Node
 const bundler = new Bundler(entry, options);
-const bundle = bundler.bundle();
+bundler.bundle();
 
 // Closes process when build ends
 bundler.on('buildEnd', () => process.exit());
