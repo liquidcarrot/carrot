405cfb132f5a9ed1a4098458aebb7b724533e950
Manuel Raimann <raimannma@outlook.de>
2020-04-01 18:31:12 +0200

    optimize imports

--- test/units/architecture/NodeTest.ts
+++ test/units/architecture/NodeTest.ts
@@ -1,5 +1,5 @@
 import {Node, NodeJSON, NodeType} from "../../../src/architecture/Node";
-import {Connection} from "../../../src/architecture/Connection";
+import {Connection} from "../../../src";
 import {randDouble, randInt} from "../../../src/methods/Utils";
 import {ModBiasMutation} from "../../../src/methods/Mutation";
 import {expect} from "chai";
