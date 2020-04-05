d79c3864c1adb15a1c4d3b4c6acb91f2ec8f353b
Manuel Raimann <raimannma@outlook.de>
2020-04-06 01:05:47 +0200

    Test function for creating a multilayer network

--- test/units/architecture/ArchitectTest.ts
+++ test/units/architecture/ArchitectTest.ts
@@ -1,13 +1,26 @@
-/**
- *
- * There are 5 questions every unit test must answer.
- *
- * What is the unit under test (module, function, class, whatever)?
- * What should it do? (Prose description)
- * What was the actual output?
- * What was the expected output?
- * How do you reproduce the failure?
- *
- */
-// it('test method', ()=>{
-// });
+import {ActivationType} from "../../../src/methods/Activation";
+import {Architect} from "../../../src/architecture/Architect";
+import {OutputLayer} from "../../../src/architecture/layer/OutputLayer";
+import {InputLayer} from "../../../src/architecture/layer/InputLayer";
+import {DenseLayer} from "../../../src/architecture/layer/DenseLayer";
+import {Network} from "../../../src/architecture/Network";
+import {expect} from "chai";
+
+describe("ArchitectTest", () => {
+    it("Build Multilayer-Perceptron", () => {
+        const architect: Architect = new Architect();
+
+        architect.addLayer(new InputLayer(10));
+        architect.addLayer(new DenseLayer(10, {activationType: ActivationType.RELUActivation}));
+        architect.addLayer(new DenseLayer(50, {activationType: ActivationType.RELUActivation}));
+        architect.addLayer(new DenseLayer(20, {activationType: ActivationType.RELUActivation}));
+        architect.addLayer(new DenseLayer(10, {activationType: ActivationType.RELUActivation}));
+        architect.addLayer(new OutputLayer(2));
+
+        const network: Network = architect.buildModel();
+
+        expect(network.nodes.length).to.be.equal(10 + 10 + 50 + 20 + 10 + 2);
+        expect(network.connections.length).to.be.equal(10 * 10 + 10 * 50 + 50 * 20 + 20 * 10 + 10 * 2);
+        expect(network.gates.length).to.be.equal(0);
+    });
+});
