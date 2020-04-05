cbb1a9dfadef037f3bc320b6c67ce3d5669f91d2
Manuel Raimann <raimannma@outlook.de>
2020-04-06 01:56:40 +0200

    adding a test function for training a multilayer network

--- test/units/architecture/ArchitectTest.ts
+++ test/units/architecture/ArchitectTest.ts
@@ -5,6 +5,7 @@ import {InputLayer} from "../../../src/architecture/layer/InputLayer";
 import {DenseLayer} from "../../../src/architecture/layer/DenseLayer";
 import {Network} from "../../../src/architecture/Network";
 import {expect} from "chai";
+import {BinaryLoss} from "../../../src/methods/Loss";
 
 describe("ArchitectTest", () => {
     it("Build Multilayer-Perceptron", () => {
@@ -22,5 +23,28 @@ describe("ArchitectTest", () => {
         expect(network.nodes.length).to.be.equal(10 + 10 + 50 + 20 + 10 + 2);
         expect(network.connections.length).to.be.equal(10 * 10 + 10 * 50 + 50 * 20 + 20 * 10 + 10 * 2);
         expect(network.gates.length).to.be.equal(0);
+    });
+
+    it("Train Multilayer-Perceptron", () => {
+        const architect: Architect = new Architect();
+
+        architect.addLayer(new InputLayer(2));
+        architect.addLayer(new DenseLayer(5, {activationType: ActivationType.RELUActivation}));
+        architect.addLayer(new OutputLayer(1));
+
+        const network: Network = architect.buildModel();
+
+        const AND_GATE: { input: number[], output: number[] }[] = [
+            {input: [0, 0], output: [0]},
+            {input: [0, 1], output: [0]},
+            {input: [1, 0], output: [0]},
+            {input: [1, 1], output: [1]}
+        ];
+
+        network.train(AND_GATE, {
+            error: 0.001,
+            loss: new BinaryLoss(),
+            shuffle: true,
+        });
     });
 });
