181749e1b4763d08ce52e2dcef777d033cd270b9
Manuel Raimann <raimannma@outlook.de>
2020-04-06 14:14:27 +0200

    one activation reference for all nodes in dense layer

--- src/architecture/layer/DenseLayer.ts
+++ src/architecture/layer/DenseLayer.ts
@@ -6,9 +6,11 @@ export class DenseLayer extends Layer {
     constructor(outputSize: number, options: { activationType?: ActivationType } = {}) {
         super(outputSize);
 
+        const activation: Activation = Activation.getActivation(options.activationType ?? ActivationType.LogisticActivation);
+
         for (let i: number = 0; i < outputSize; i++) {
             const node: Node = new Node(NodeType.HIDDEN);
-            node.squash = Activation.getActivation(options.activationType ?? ActivationType.LogisticActivation);
+            node.squash = activation;
             this.nodes.push(node);
             this.inputNodes.add(node);
             this.outputNodes.add(node);
