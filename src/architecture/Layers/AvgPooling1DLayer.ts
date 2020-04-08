539a6e4ad1f51e0bd9d21ac5d4a2149229127560
Manuel Raimann <raimannma@outlook.de>
2020-04-08 19:12:25 +0200

    implement Min, Max and Avg Pooling Layers for 1D Data

---
+++ src/architecture/Layers/AvgPooling1DLayer.ts
@@ -0,0 +1,19 @@
+import {PoolNode} from "../Nodes/PoolNode";
+import {PoolingType} from "../../enums/PoolingType";
+import {PoolingLayer} from "./PoolingLayer";
+import {ActivationType} from "../../enums/ActivationType";
+
+export class AvgPooling1DLayer extends PoolingLayer {
+    constructor(outputSize: number, options: { activationType?: ActivationType } = {}) {
+        super(outputSize);
+
+        const activationType: ActivationType = options.activationType ?? ActivationType.IdentityActivation;
+
+        for (let i: number = 0; i < outputSize; i++) {
+            this.inputNodes.add(new PoolNode(PoolingType.AVG_POOLING).setSquash(activationType));
+        }
+
+        this.outputNodes = this.inputNodes;
+        this.nodes.push(...Array.from(this.inputNodes));
+    }
+}
