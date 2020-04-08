e0484f94d8eb9edbef02c165467455eadc481c7c
Manuel Raimann <raimannma@outlook.de>
2020-04-08 22:42:39 +0200

    add more options to GaussianNoiseNode

--- src/architecture/Nodes/GaussianNoiseNode.ts
+++ src/architecture/Nodes/GaussianNoiseNode.ts
@@ -2,15 +2,19 @@ import {avg, generateGaussian} from "../../methods/Utils";
 import {ConstantNode} from "./ConstantNode";
 
 export class GaussianNoiseNode extends ConstantNode {
+    private readonly mean: number;
+    private readonly deviation: number;
 
-    constructor() {
+    constructor(options: { mean?: number, deviation?: number }) {
         super();
+        this.mean = options.mean ?? 0;
+        this.deviation = options.deviation ?? 2;
     }
 
     public activate(): number {
         // should only be one incoming, but if there are more incoming connections take the average and add gaussian noise
         const incomingStates: number[] = this.incoming.map(conn => conn.from.activation * conn.weight * conn.gain);
-        this.state = avg(incomingStates) + generateGaussian();
+        this.state = avg(incomingStates) + generateGaussian(this.mean, this.deviation);
         this.activation = this.state;
 
         // Adjust gain
