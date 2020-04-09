0d5870962e97f4be03065f4622b01962a29fb31c
Manuel Raimann <raimannma@outlook.de>
2020-04-09 12:07:09 +0200

    adding ability to add noise directly to the input layer

--- src/architecture/Layers/CoreLayers/InputLayer.ts
+++ src/architecture/Layers/CoreLayers/InputLayer.ts
@@ -1,16 +1,24 @@
 import {Layer} from "../Layer";
 import {Node} from "../../Node";
-import {NodeType} from "../../../enums/NodeType";
+import {NodeType, NoiseNodeType} from "../../../enums/NodeType";
 import {ConnectionType} from "../../../enums/ConnectionType";
+import {NoiseLayer} from "../NoiseLayers/NoiseLayer";
 
 export class InputLayer extends Layer {
-    public constructor(outputSize: number) {
+    public constructor(outputSize: number, options: { noise?: NoiseNodeType } = {}) {
         super(outputSize);
 
         for (let i: number = 0; i < outputSize; i++) {
             const node: Node = new Node(NodeType.INPUT);
             this.nodes.push(node);
-            this.outputNodes.add(node);
+        }
+
+        if (options.noise) {
+            const noiseLayer: NoiseLayer = new NoiseLayer(options.noise ?? NoiseNodeType.GAUSSIAN_NOISE);
+            noiseLayer.outputNodes.forEach(node => this.outputNodes.add(node));
+            this.connections.push(...Layer.connect(this.nodes, noiseLayer, noiseLayer.getDefaultIncomingConnectionType()));
+        } else {
+            this.nodes.forEach(node => this.outputNodes.add(node));
         }
     }
 
