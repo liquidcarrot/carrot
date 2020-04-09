37c7c6dab08c3e356341b1fb2ea886d05b98a221
Manuel Raimann <raimannma@outlook.de>
2020-04-09 12:51:01 +0200

    remove HopfieldLayer.ts for now as it is not implemented yet

--- src/architecture/Layers/RecurrentLayers/HopfieldLayer.ts
+++
@@ -1,8 +0,0 @@
-import {DenseLayer} from "../CoreLayers/DenseLayer";
-
-export class HopfieldLayer extends DenseLayer {
-    constructor(outputSize: number) {
-        // TODO: Implement it
-        super(outputSize);
-    }
-}
