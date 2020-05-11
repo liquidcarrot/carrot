b457c27550bbb8dd3705633b2903246b84e55f0f
Manuel Raimann <raimannma@outlook.de>
2020-05-11 15:40:36 +0200

    adding docs for NodeType.ts

--- src/enums/NodeType.ts
+++ src/enums/NodeType.ts
@@ -1,11 +1,45 @@
+/**
+ * The type of node.
+ */
 export enum NodeType {
-    INPUT, HIDDEN, OUTPUT, POOL_NODE
+    /**
+     * Node is an input node.
+     */
+    INPUT,
+    /**
+     * Node is a hidden node.
+     */
+    HIDDEN,
+    /**
+     * Node is a output node.
+     */
+    OUTPUT
 }
 
+/**
+ * The type of pool node.
+ */
 export enum PoolNodeType {
-    MAX_POOLING, AVG_POOLING, MIN_POOLING
+    /**
+     * Maximum pooling node.
+     */
+    MAX_POOLING,
+    /**
+     * Average pooling node.
+     */
+    AVG_POOLING,
+    /**
+     * Minimum pooling node.
+     */
+    MIN_POOLING
 }
 
+/**
+ * The type of noise node.
+ */
 export enum NoiseNodeType {
+    /**
+     * Gaussian noise node
+     */
     GAUSSIAN_NOISE
 }
