1a030436bd872b6f2a1ca134a138289cf2790122
Manuel Raimann <raimannma@outlook.de>
2020-05-11 22:14:30 +0200

    adding docs for NodeJSON.ts

--- src/interfaces/NodeJSON.ts
+++ src/interfaces/NodeJSON.ts
@@ -1,18 +1,48 @@
 import {ActivationType} from "../enums/ActivationType";
 import {PoolNodeType} from "../enums/NodeType";
 
+/**
+ * An interface for representing a node with an json object
+ */
 export interface NodeJSON {
+    /**
+     * The bias value of the node
+     */
     bias?: number;
+    /**
+     * The type of this node
+     */
     type?: number;
+    /**
+     * The activation type of this node
+     */
     squash?: ActivationType;
+    /**
+     * The mask value of this node
+     */
     mask?: number;
+    /**
+     * The index of this node
+     */
     index?: number;
 }
 
+/**
+ * An interface for representing a pool node with an json object
+ */
 export interface PoolNodeJSON extends NodeJSON {
+    /**
+     * The poolType of this pooling node
+     */
     poolType: PoolNodeType;
 }
 
+/**
+ * An interface for representing a dropout node with an json object
+ */
 export interface DropoutNodeJSON extends NodeJSON {
+    /**
+     * The dropout probability of this dropout node
+     */
     probability: number;
 }
