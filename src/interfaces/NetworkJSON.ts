1af68a53794713c3477844ec4b5194ce170d7966
Manuel Raimann <raimannma@outlook.de>
2020-05-11 22:27:19 +0200

    adding docs for NetworkJSON.ts

--- src/interfaces/NetworkJSON.ts
+++ src/interfaces/NetworkJSON.ts
@@ -1,9 +1,24 @@
 import {ConnectionJSON} from "./ConnectionJSON";
 import {NodeJSON} from "./NodeJSON";
 
+/**
+ * An interface for representing a network with an json object
+ */
 export interface NetworkJSON {
+    /**
+     * All nodes inside the network
+     */
     nodes: NodeJSON[];
+    /**
+     * All connections in the network
+     */
     connections: ConnectionJSON[];
+    /**
+     * The input size of the network
+     */
     inputSize: number;
+    /**
+     * The output size of the network
+     */
     outputSize: number;
 }
