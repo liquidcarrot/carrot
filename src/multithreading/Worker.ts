795ae8abf6a38c0cf9274bae843c1f4d9cc88859
Manuel Raimann <raimannma@outlook.de>
2020-05-18 09:40:15 +0200

    make expose function not async

--- src/multithreading/Worker.ts
+++ src/multithreading/Worker.ts
@@ -2,9 +2,6 @@ import {expose} from "threads/worker";
 import {Network} from "../architecture/Network";
 import {ALL_LOSSES, MSELoss} from "../methods/Loss";
 
-expose(async (serializedDataSet: string, jsonNetwork: string, lossIndex: number): Promise<number> => {
-    return new Promise(resolve => {
-        // parse network and dataset and run test
-        resolve(Network.fromJSON(JSON.parse(jsonNetwork)).test(JSON.parse(serializedDataSet), ALL_LOSSES[lossIndex] ?? new MSELoss()));
-    });
+expose((serializedDataSet: string, jsonNetwork: string, lossIndex: number): number => {
+    return Network.fromJSON(JSON.parse(jsonNetwork)).test(JSON.parse(serializedDataSet), ALL_LOSSES[lossIndex] ?? new MSELoss());
 });
