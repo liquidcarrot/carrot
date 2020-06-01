eb04106873f41028bc8d5b7186e5756454d9621b
Manuel Raimann <raimannma@outlook.de>
2020-06-01 08:59:59 +0200

    fix rate tests

--- test/units/methods/RateTest.ts
+++ test/units/methods/RateTest.ts
@@ -1,34 +1,46 @@
 import {expect} from "chai";
-import {randInt} from "../../../src/methods/Utils";
 import {ExponentialRate, FixedRate, InverseRate, StepRate} from "../../../src/methods/Rate";
+import {randInt} from "../../../src/methods/Utils";
 
 describe("Rate", () => {
-    describe('FixedRate', () => {
+    it('FixedRate', () => {
         const baseRate: number = Math.random();
         const iteration: number = randInt(1, 100);
-        expect(new FixedRate(baseRate).calc(iteration)).to.be.a("number");
+
+        const fixedRate: FixedRate = new FixedRate(baseRate);
+        expect(fixedRate.calc(iteration)).to.be.a("number");
+        expect(fixedRate.calc(iteration + 10)).to.be.equal(fixedRate.calc(iteration));
     });
 
-    describe('StepRate', () => {
+    it('StepRate', () => {
         const baseRate: number = Math.random();
         const iteration: number = randInt(1, 100);
         const gamma: number = Math.random();
         const stepSize: number = randInt(1, 100);
-        expect(new StepRate(baseRate, gamma, stepSize).calc(iteration)).to.be.a("number");
+
+        const stepRate: StepRate = new StepRate(baseRate, gamma, stepSize);
+        expect(stepRate.calc(iteration)).to.be.a("number");
+        expect(stepRate.calc(iteration + 10)).to.be.at.most(stepRate.calc(iteration));
     });
 
-    describe('ExponentialRate', () => {
+    it('ExponentialRate', () => {
         const baseRate: number = Math.random();
         const iteration: number = randInt(1, 100);
         const gamma: number = Math.random();
-        expect(new ExponentialRate(baseRate, gamma).calc(iteration)).to.be.a("number");
+
+        const exponentialRate: ExponentialRate = new ExponentialRate(baseRate, gamma);
+        expect(exponentialRate.calc(iteration)).to.be.a("number");
+        expect(exponentialRate.calc(iteration + 10)).to.be.below(exponentialRate.calc(iteration));
     });
 
-    describe('InverseRate', () => {
+    it('InverseRate', () => {
         const baseRate: number = Math.random();
         const iteration: number = randInt(1, 100);
         const gamma: number = Math.random();
         const power: number = randInt(1, 100);
-        expect(new InverseRate(baseRate, gamma, power).calc(iteration)).to.be.a("number");
+
+        const inverseRate: InverseRate = new InverseRate(baseRate, gamma, power);
+        expect(inverseRate.calc(iteration)).to.be.a("number");
+        expect(inverseRate.calc(iteration + 10)).to.be.below(inverseRate.calc(iteration));
     });
 });
