3498fd89e08c6f608715f7d0d7273611b2e5047c
Manuel Raimann <raimannma@outlook.de>
2020-06-01 11:13:13 +0200

    fix test titles

--- test/units/methods/ActivationTest.ts
+++ test/units/methods/ActivationTest.ts
@@ -20,173 +20,173 @@ import {
 import {randDouble} from "../../../src/methods/Utils";
 
 describe("Activation", () => {
-    describe("activation.LOGISTIC()", () => {
-        it("activation.LOGISTIC(number, derivative=false) => {number}", () => {
+    describe("LogisticActivation", () => {
+        it("LogisticActivation(number, derivative=false) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(LogisticActivation(x, false)).to.equal(1 / (1 + Math.exp(-x)));
         });
-        it("activation.LOGISTIC(number, derivative=true) => {number}", () => {
+        it("LogisticActivation(number, derivative=true) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(LogisticActivation(x, true)).to.equal(LogisticActivation(x, false) * (1 - LogisticActivation(x, false)));
         });
     });
-    describe("activation.TANH()", () => {
-        it("activation.TANH(number, derivative=false) => {number}", () => {
+    describe("TanhActivation()", () => {
+        it("TanhActivation(number, derivative=false) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(TanhActivation(x, false)).to.equal(Math.tanh(x));
         });
-        it("activation.TANH(number, derivative=true) => {number}", () => {
+        it("TanhActivation(number, derivative=true) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(TanhActivation(x, true)).to.equal(1 - Math.pow(Math.tanh(x), 2));
         });
     });
-    describe("activation.IDENTITY()", () => {
-        it("activation.IDENTITY(number, derivative=false) => {number}", () => {
+    describe("IdentityActivation", () => {
+        it("IdentityActivation(number, derivative=false) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(IdentityActivation(x, false)).to.equal(x);
         });
-        it("activation.IDENTITY(number, derivative=true) => {number}", () => {
+        it("IdentityActivation(number, derivative=true) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(IdentityActivation(x, true)).to.equal(1);
         });
     });
-    describe("activation.STEP()", () => {
-        it("activation.STEP(number) => {number}", () => {
+    describe("StepActivation", () => {
+        it("StepActivation(number) => {number}", () => {
             const x: number = randDouble(-50, 50);
             const z: number = x > 0 ? 1 : 0;
             expect(StepActivation(x, false)).to.equal(z);
         });
-        it("activation.STEP(number, derivative=true) => {number}", () => {
+        it("StepActivation(number, derivative=true) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(StepActivation(x, true)).to.equal(0);
         });
     });
-    describe("activation.RELU()", () => {
-        it("activation.RELU(number) => {number, derivative=false}", () => {
+    describe("RELUActivation", () => {
+        it("RELUActivation(number) => {number, derivative=false}", () => {
             const x: number = randDouble(-50, 50);
             const z: number = x > 0 ? x : 0;
             expect(RELUActivation(x, false)).to.equal(z);
         });
-        it("activation.RELU(number, derivative=true) => {number}", () => {
+        it("RELUActivation(number, derivative=true) => {number}", () => {
             const x: number = randDouble(-50, 50);
             const z: number = x > 0 ? 1 : 0;
             expect(RELUActivation(x, true)).to.equal(z);
         });
     });
-    describe("activation.SOFTSIGN()", () => {
-        it("activation.SOFTSIGN(number, derivative=false) => {number}", () => {
+    describe("SoftSignActivation", () => {
+        it("SoftSignActivation(number, derivative=false) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(SoftSignActivation(x, false)).to.equal(x / (1 + Math.abs(x)));
         });
-        it("activation.SOFTSIGN(number, derivative=true) => {number}", () => {
+        it("SoftSignActivation(number, derivative=true) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(SoftSignActivation(x, true)).to.equal((x / Math.pow(1 + Math.abs(x), 2)));
         });
     });
-    describe("activation.SINUSOID()", () => {
-        it("activation.SINUSOID(number, derivative=false) => {number}", () => {
+    describe("SinusoidActivation", () => {
+        it("SinusoidActivation(number, derivative=false) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(SinusoidActivation(x, false)).to.equal(Math.sin(x));
         });
-        it("activation.SINUSOID(number, derivative=true) => {number}", () => {
+        it("SinusoidActivation(number, derivative=true) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(SinusoidActivation(x, true)).to.equal(Math.cos(x));
         });
     });
-    describe("activation.GAUSSIAN()", () => {
-        it("activation.GAUSSIAN(number) => {number, derivative=false}", () => {
+    describe("GaussianActivation", () => {
+        it("GaussianActivation(number) => {number, derivative=false}", () => {
             const x: number = randDouble(-50, 50);
             expect(GaussianActivation(x, false)).to.equal(Math.exp(-Math.pow(x, 2)));
         });
-        it("activation.GAUSSIAN(number, derivative=true) => {number}", () => {
+        it("GaussianActivation(number, derivative=true) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(GaussianActivation(x, true)).to.equal(-2 * x * Math.exp(-Math.pow(x, 2)));
         });
     });
-    describe("activation.BENT_IDENTITY()", () => {
-        it("activation.BENT_IDENTITY(number, derivative=false) => {number}", () => {
+    describe("BentIdentityActivation", () => {
+        it("BentIdentityActivation(number, derivative=false) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(BentIdentityActivation(x, false)).to.equal((Math.sqrt(Math.pow(x, 2) + 1) - 1) / 2 + x);
         });
-        it("activation.BENT_IDENTITY(number, derivative=true) => {number}", () => {
+        it("BentIdentityActivation(number, derivative=true) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(BentIdentityActivation(x, true)).to.equal(x / (2 * Math.sqrt(Math.pow(x, 2) + 1)) + 1);
         });
     });
-    describe("activation.BIPOLAR()", () => {
-        it("activation.BIPOLAR(number, derivative=false) => {number}", () => {
+    describe("BipolarActivation", () => {
+        it("BipolarActivation(number, derivative=false) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(BipolarActivation(x, false)).to.equal(x > 0 ? 1 : -1);
         });
-        it("activation.BIPOLAR(number, derivative=true) => {number}", () => {
+        it("BipolarActivation(number, derivative=true) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(BipolarActivation(x, true)).to.equal(0);
         });
     });
-    describe("activation.BIPOLAR_SIGMOID()", () => {
-        it("activation.BIPOLAR_SIGMOID(number, derivative=false) => {number}", () => {
+    describe("BipolarSigmoidActivation", () => {
+        it("BipolarSigmoidActivation(number, derivative=false) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(BipolarSigmoidActivation(x, false)).to.equal(2 / (1 + Math.exp(-x)) - 1);
         });
-        it("activation.BIPOLAR_SIGMOID(number, derivative=true) => {number}", () => {
+        it("BipolarSigmoidActivation(number, derivative=true) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(BipolarSigmoidActivation(x, true)).to.equal((2 * Math.exp(-x)) / Math.pow(1 + Math.exp(-x), 2));
         });
     });
-    describe("activation.HARD_TANH()", () => {
-        it("activation.HARD_TANH(number) => {number, derivative=false}", () => {
+    describe("HardTanhActivation", () => {
+        it("HardTanhActivation(number) => {number, derivative=false}", () => {
             const x: number = randDouble(-50, 50);
             expect(HardTanhActivation(x, false)).to.equal(Math.max(-1, Math.min(1, x)));
         });
-        it("activation.HARD_TANH(number, derivative=true) => {number}", () => {
+        it("HardTanhActivation(number, derivative=true) => {number}", () => {
             const x: number = randDouble(-50, 50);
             const z: number = x > -1 && x < 1 ? 1 : 0;
             expect(HardTanhActivation(x, true)).to.equal(z);
         });
     });
-    describe("activation.ABSOLUTE()", () => {
-        it("activation.ABSOLUTE(number, derivative=false) => {number}", () => {
+    describe("AbsoluteActivation", () => {
+        it("AbsoluteActivation(number, derivative=false) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(AbsoluteActivation(x, false)).to.equal(Math.abs(x));
         });
-        it("activation.ABSOLUTE(number, derivative=true) => {number}", () => {
+        it("AbsoluteActivation(number, derivative=true) => {number}", () => {
             const x: number = randDouble(-50, 50);
             const z: number = x < 0 ? -1 : 1;
             expect(AbsoluteActivation(x, true)).to.equal(z);
         });
     });
-    describe("activation.INVERSE()", () => {
-        it("activation.INVERSE(number, derivative=false) => {number}", () => {
+    describe("InverseActivation", () => {
+        it("InverseActivation(number, derivative=false) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(InverseActivation(x, false)).to.equal(1 - x);
         });
-        it("activation.INVERSE(number, derivative=true) => {number}", () => {
+        it("InverseActivation(number, derivative=true) => {number}", () => {
             const x: number = randDouble(-50, 50);
             expect(InverseActivation(x, true)).to.equal(-1);
         });
     });
-    describe("activation.SELU()", () => {
+    describe("SELUActivation", () => {
         const alpha: number = 1.6732632423543772848170429916717;
         const scale: number = 1.0507009873554804934193349852946;
 
-        it("activation.SELU(number, derivative=false) => {number}", () => {
+        it("SELUActivation(number, derivative=false) => {number}", () => {
             const x: number = randDouble(-50, 50);
             const z: number = (x > 0 ? x : alpha * Math.exp(x) - alpha) * scale;
             expect(SELUActivation(x, false)).to.be.closeTo(z, 0.01);
         });
-        it("activation.SELU(number, derivative=true) => {number}", () => {
+        it("SELUActivation(number, derivative=true) => {number}", () => {
             const x: number = randDouble(-50, 50);
             const z: number = x > 0 ? scale : ((x > 0 ? x : alpha * Math.exp(x) - alpha) + alpha) * scale;
             expect(SELUActivation(x, true)).to.be.closeTo(z, 0.01);
         });
     });
-    describe("activation.MISH()", () => {
-        it("activation.MISH(number, derivative=false) => {number}", () => {
+    describe("MISHActivation", () => {
+        it("MISHActivation(number, derivative=false) => {number}", () => {
             const x: number = randDouble(-50, 50);
             const z: number = x * Math.tanh(Math.log(1 + Math.exp(x)));
             expect(MISHActivation(x, false)).to.be.closeTo(z, 0.01);
         });
-        it("activation.MISH(number, derivative=true) => {number}", () => {
+        it("MISHActivation(number, derivative=true) => {number}", () => {
             const x: number = randDouble(-50, 50);
 
             const ex: number = Math.exp(x);
