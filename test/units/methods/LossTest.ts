9164f8c3b8abcf165c2f53242a9902758ac8c202
Manuel Raimann <raimannma@outlook.de>
2020-06-01 09:09:14 +0200

    refactor loss tests

--- test/units/methods/LossTest.ts
+++ test/units/methods/LossTest.ts
@@ -1,15 +1,16 @@
-import {randDouble} from "../../../src/methods/Utils";
 import {expect} from "chai";
 import {ALL_LOSSES} from "../../../src/methods/Loss";
+import {randDouble} from "../../../src/methods/Utils";
 
 describe("Loss", () => {
     ALL_LOSSES.forEach(loss => {
-        describe(`Loss.${loss}()`, () => {
+        describe(`Loss.${loss.constructor.name}()`, () => {
             const targets: number[] = [randDouble(-50, 50), randDouble(-50, 50), randDouble(-50, 50)];
             const outputs: number[] = [randDouble(-50, 50), randDouble(-50, 50), randDouble(-50, 50)];
 
-            it(`Loss.${loss}(targets=[${targets}], outputs=[${outputs}]) => {number}`, () => {
+            it(`Loss.${loss.constructor.name}(targets=[${targets}], outputs=[${outputs}]) => {number}`, () => {
                 expect(loss.calc(targets, outputs)).to.be.a("number");
+                expect(Number.isFinite(loss.calc(targets, outputs))).to.be.true;
             });
         });
     });
