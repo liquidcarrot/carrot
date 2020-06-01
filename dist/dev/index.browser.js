222946d0166632badc2239271a3ea0f81e4f370a
raimannma <raimannma@users.noreply.github.com>
2020-06-01 13:35:33 +0000

    Apply automatic changes

--- dist/dev/index.browser.js
+++ dist/dev/index.browser.js
@@ -7268,7 +7268,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44769" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34461" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
