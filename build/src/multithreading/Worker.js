abd15bd494f7e4542c8651f7530565ec386fb183
Manuel Raimann <raimannma@outlook.de>
2020-06-09 01:42:13 +0200

    clear build directory

--- build/src/multithreading/Worker.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var worker_1 = require("threads/worker");
-var Network_1 = require("../architecture/Network");
-var Loss_1 = require("../methods/Loss");
-worker_1.expose(function (serializedDataSet, jsonNetwork, lossIndex) {
-    return Network_1.Network.fromJSON(JSON.parse(jsonNetwork)).test(JSON.parse(serializedDataSet), Object.values(Loss_1.ALL_LOSSES)[lossIndex]);
-});
