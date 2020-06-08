2e23dc3761ee14430e59c22e44216a1e711c6ab8
Manuel Raimann <raimannma@outlook.de>
2020-06-08 14:19:14 +0200

    add time complexity info to Node.ts

--- src/architecture/Node.ts
+++ src/architecture/Node.ts
@@ -116,9 +116,9 @@ export class Node {
      * Convert a json object to a node
      *
      * @param json A node represented as a JSON object
+     * @time O(1)
      *
      * @returns itself
-     *
      */
     public fromJSON(json: NodeJSON): Node {
         this.bias = json.bias ?? randDouble(-1, 1);
@@ -151,7 +151,7 @@ export class Node {
      * Mutates the node's bias
      *
      * @param method The method is needed for the min and max value of the node's bias otherwise a range of [-1,1] is chosen
-     *
+     * @time O(1)
      */
     public mutateBias(method: ModBiasMutation = new ModBiasMutation()): void {
         this.bias += randDouble(method.min, method.max); // add a random value in range [min,max)
@@ -159,6 +159,7 @@ export class Node {
 
     /**
      * Mutates the node's activation function
+     * @time O(n)
      */
     public mutateActivation(allowedActivations: activationType[] = Object.values(ALL_ACTIVATIONS)): void {
         // pick a random activation from allowed activations except the current activation
@@ -172,6 +173,7 @@ export class Node {
      * Checks if the given node(s) are have outgoing connections to this node
      *
      * @param node Checks if `node(s)` have outgoing connections into this node
+     * @time O(n)
      *
      * @return Returns true, if every node(s) has an outgoing connection into this node
      */
@@ -187,6 +189,7 @@ export class Node {
      * Checks if this node has an outgoing connection(s) into the given node(s)
      *
      * @param node Checks if this node has outgoing connection(s) into `node(s)`
+     * @time O(n)
      *
      * @returns Returns true, if this node has an outgoing connection into every node(s)
      */
@@ -202,6 +205,7 @@ export class Node {
      * This node gates (influences) the given connection
      *
      * @param connection Connection to be gated (influenced) by a neuron
+     * @time O(1)
      */
     public addGate(connection: Connection): void {
         this.gated.add(connection);
@@ -212,6 +216,7 @@ export class Node {
      * Stops this node from gating (manipulating) the given connection(s)
      *
      * @param connection Connections to remove gate - _i.e. remove this node from_
+     * @time O(1)
      */
     public removeGate(connection: Connection): void {
         this.gated.delete(connection);
@@ -225,6 +230,7 @@ export class Node {
      * @param target Node(s) to project connection(s) to
      * @param weight Initial connection(s) [weight](https://en.wikipedia.org/wiki/Synaptic_weight)
      * @param twoSided If `true` connect nodes to each other
+     * @time O(n)
      */
     public connect(target: Node, weight: number = 1, twoSided: boolean = false): Connection {
         if (target === this) { // self connection
@@ -251,6 +257,7 @@ export class Node {
      *
      * @param node Node(s) to remove connection(s) to
      * @param twoSided=false If `true` disconnects nodes from each other (i.e. both sides)
+     * @time O(n)
      */
     public disconnect(node: Node, twoSided: boolean = false): Connection {
         if (node === this) { // self connection
@@ -288,6 +295,7 @@ export class Node {
      *
      * @param target The target value (i.e. "the value the network SHOULD have given")
      * @param options More options for propagation
+     * @time O(n&sup2;)
      *
      * @see [Regularization Neataptic](https://wagenaartje.github.io/neataptic/docs/methods/regularization/)
      * @see [What is backpropagation | YouTube](https://www.youtube.com/watch?v=Ilg3gGewQ5U)
@@ -372,6 +380,7 @@ export class Node {
      *
      * @param [input] Environment signal (i.e. optional numerical value passed to the network as input)  - _should only be passed in input neurons_
      * @param [trace] Controls whether traces are created when activation happens (a trace is meta information left behind for different uses, e.g. backpropagation).
+     * @time O(n&sup2;)
      *
      * @returns A neuron's ['Squashed'](https://medium.com/the-theory-of-everything/understanding-activation-functions-in-neural-networks-9491262884e0) output value
      */
@@ -454,6 +463,7 @@ export class Node {
     /**
      * Converts the node to a json object that can later be converted back
      *
+     * @time O(1)
      * @returns A node representing json object
      */
     public toJSON(): NodeJSON {
@@ -468,6 +478,7 @@ export class Node {
 
     /**
      * Is this a input Node?
+     * @time O(1)
      */
     public isInputNode(): boolean {
         return this.type === NodeType.INPUT;
@@ -475,6 +486,7 @@ export class Node {
 
     /**
      * Is this a hidden Node?
+     * @time O(1)
      */
     public isHiddenNode(): boolean {
         return this.type === NodeType.HIDDEN;
@@ -482,6 +494,7 @@ export class Node {
 
     /**
      * Is this a output Node?
+     * @time O(1)
      */
     public isOutputNode(): boolean {
         return this.type === NodeType.OUTPUT;
@@ -491,6 +504,7 @@ export class Node {
      * Set bias.
      *
      * @param bias the new bias value
+     * @time O(1)
      */
     public setBias(bias: number): Node {
         this.bias = bias;
@@ -501,6 +515,7 @@ export class Node {
      * Set activation type
      *
      * @param activation the new activation type
+     * @time O(1)
      */
     public setActivationType(activation: activationType): Node {
         this.squash = activation;
